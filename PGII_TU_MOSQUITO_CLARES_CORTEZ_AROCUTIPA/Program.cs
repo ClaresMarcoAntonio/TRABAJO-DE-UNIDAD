29cfbeab98e403dc5df5d8cb5154b6fd63441df9
MarcoClaresChura <marclaresc@upt.pe>
2025-09-09 23:34:12 -0500

    Version2

---
+++ PGII_TU_MOSQUITO_CLARES_CORTEZ_AROCUTIPA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PGII_TU_MOSQUITO_CLARES_CORTEZ_AROCUTIPA.Ejercicios;
+
+namespace PGII_TU_MOSQUITO_CLARES_CORTEZ_AROCUTIPA
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Ejercicio_06());
+        }
+    }
+}
